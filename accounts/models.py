5d30c11476bce68dc5301a7776375aafc4f4a45a
AbdAllah Khodeir <abdallahkhodeir@gmail.com>
2023-07-07 00:10:42 -0600

    fixed the moel for the accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib import auth
 from django.db import models
+from django.utils import timezone
 
 
-# Create your models here.
-
-class User(AbstractUser):
+class User(auth.models.User, auth.models.PermissionsMixin):
 
     def __str__(self):
         return "@{}".format(self.username)
